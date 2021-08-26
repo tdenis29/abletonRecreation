a1271be053d2c33ba89d3493c8762123e3ca0afe
tdenis29 <tdenis29@outlook.com>
2021-08-26 13:26:25 -0700

    update/mobileactivestates + nav

---
+++ app.js
@@ -0,0 +1,6 @@
+//modal
+
+document.getElementById('hamburger').addEventListener('click', (e) => {
+    let overlay = document.getElementById('overlay');
+    overlay.classList.toggle('hidden');
+})
