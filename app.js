3f4974ef9b9e255f463eee99604da88ff73b9143
tdenis29 <tdenis29@outlook.com>
2021-08-26 14:54:15 -0700

    update/mobiledone

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 //modal
+let hamburger = document.getElementById('hamburger');
+let modalclosed = true;
 
-document.getElementById('hamburger').addEventListener('click', (e) => {
+hamburger.addEventListener('click', (e) => {
     let overlay = document.getElementById('overlay');
+    let logosvg = document.getElementById('logosvg');
+    let menusvg = document.getElementById('menusvg');
+    logosvg.classList.toggle('modalactive');
     overlay.classList.toggle('hidden');
+    modalclosed = !modalclosed;
+    if(modalclosed === false){
+        menusvg.classList.add('modalactive');
+    } else{
+        menusvg.classList.remove('modalactive');
+    }
 })
