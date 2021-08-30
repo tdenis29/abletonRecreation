c1271b464692b5645470a549c692a7700b51a391
tdenis29 <tdenis29@outlook.com>
2021-08-30 10:18:21 -0700

    update/tabsdone now invertsvgarrow

--- app.js
+++ app.js
@@ -16,24 +16,22 @@ hamburger.addEventListener('click', (e) => {
     }
 });
 //tabs
-function openTab(evt, tabName) {
-    // Get all elements with class="tabcontent" and hide them
-    let tabcontent = document.getElementsByClassName("tabcontent");
-    for (let i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    let tablinks = document.getElementsByClassName("tablinks");
-    for (let i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" activate", "");
-    }
-    let tabimg = document.getElementsByClassName('tabimg');
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(tabName).classList.add("activate");
-    console.log(tabName)
-    evt.currentTarget.className += " activated";
+
+function openTab(evt, tabNum) {
+  // Get all elements with class="tabcontainer" and hide them
+  let tabcontainers = document.getElementsByClassName("tabcontainer");
+  for (let i = 0; i < tabcontainers.length; i++) {
+    tabcontainers[i].style.display = "none";
   }
 
+  // Get all elements with class="tablinks" and remove the class "active"
+  let tablinks = document.getElementsByClassName("tablinks");
+  for (let i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" activated", "");
+  }
 
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabNum).style.display = "flex";
+  evt.currentTarget.className += " activated";
+}
+document.getElementById("tab1link").click();
